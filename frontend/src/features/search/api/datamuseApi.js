0121908a35a5ead82dab26867cb35f8707cd4c50
ElPridro <ammarhadid2005@gmail.com>
2026-06-03 20:45:19 +0200

    fix: Limit Datamuse suggestion to just one word

--- frontend/src/features/search/api/datamuseApi.js
+++ frontend/src/features/search/api/datamuseApi.js
@@ -1,11 +1,14 @@
 export const fetchSuggestions = async (query) => {
-    const response = await fetch(`https://api.datamuse.com/sug?s=${encodeURIComponent(query)}&max=5`);
+    const response = await fetch(`https://api.datamuse.com/sug?s=${encodeURIComponent(query)}&max=20`);
 
     if (!response.ok) {
         throw new Error('Could not load suggestions');
     }
 
-    const results = await response.json();
+    const data = await response.json();
 
-    return results.map(i => i.word);
-}
+    return data
+        .map(i => i.word)
+        .filter(word => !word.includes(" "))
+        .slice(0, 5);
+};
